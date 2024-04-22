50d591f75f75f6f54ef6b5a28557f2bbc2f5ec44
Daniel Vieira <danielvie@gmail.com>
2024-04-23 01:53:41 +0200

    after P() definition 38m40s

--- lua/stackmap/init.lua
+++ lua/stackmap/init.lua
@@ -1,1 +1,23 @@
-print("ok, in stackmap now")
+local M = {}
+
+M.setup = function(opts)
+	print("Options:", opts)
+end
+
+-- function we need:
+-- - vim.keymaps.set(...) -> create new keymaps
+
+M.push = function(name, mappings) end
+
+M.pop = function(name) end
+
+--[[
+lua require("mapstack").push("debug_mode", {
+...
+})
+
+lua require("mapstack").pop("debug_mode")
+
+--]]
+
+return M
